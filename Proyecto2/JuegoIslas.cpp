17349c9addcc7985c3215dcc047d8fa268786fd7
dnunezf <david.a.nunez27@outlook.com>
2024-06-08 19:59:09 -0600

    DOCUMENTACION

--- Proyecto2/JuegoIslas.cpp
+++ Proyecto2/JuegoIslas.cpp
@@ -1,3 +1,13 @@
+/* -------------------------------------------------------------------+
+* |
+* (c) 2024 |
+* 1-1934-0740 - Ángel Arián Leiva Abarca   |
+* 1-1908-0008 - David Alberto Núñez Franco |
+* version 1.0.0 2024-06-01 |
+* |
+* -------------------------------------------------------------------+
+*/
+
 #include "JuegoIslas.h"
 
 void JuegoIslas::placeLine(Jugada& jugada, int jugador)
